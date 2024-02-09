add75d854e55d4431b390546b600b159ac9ea5b2
Volodya <volodechka00@gmail.com>
2024-02-09 22:01:59 +0300

    setup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,16 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
 }
